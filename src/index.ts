6d7970fc28fa95fc8aefbd8643d1402efc252279
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-04-07 16:37:46 -0300

    removeMethod

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Facade } from "./Facade";
-import * as readline from "readline";
 
 /*
 let rl = readline.createInterface({
@@ -11,16 +10,23 @@ console.log("");
 console.log("===== Mentorship Challenge Wesley Silva =====");
 console.log("");
 
+const answer = 0;
+
 rl.question(
   "What would you like to do? \n 1 - Add Question \n 2 - List Questions ",
   (answer) => {
     switch (answer.toLowerCase()) {
       case "1":
-        //.addQuestion(textQuestion)
+        const question = "What is the purpose of the mentorship?";
+        const facadeAdd = new Facade();
+        facadeAdd.addQuestion(question);
+        console.log(facadeAdd.findAllQuestions());
 
         console.log("Added!");
         break;
       case "2":
+        const facade = new Facade();
+        console.log(facade.findAllQuestions());
         console.log("Here they are!");
         break;
       default:
@@ -29,22 +35,21 @@ rl.question(
     rl.close();
   }
 );
-
-const question: Question = new Question("2", "Qual a proposta da mentoria?");
-console.log(question);
-
-console.log(
-  `
-the question id is ==>  ${question.questionId} and the question is ==> ${question.questionText}`
-);
-
-//console.log(getAllQuestions());
 */
 
 const question = "What is the purpose of the mentorship?";
+const question2 = "When does the mentorship start?";
+const question3 = "What skills can be developed?";
+
 const facade = new Facade();
 facade.addQuestion(question);
+facade.addQuestion(question2);
+facade.addQuestion(question3);
+
 const firstQuestionId = facade.findAllQuestions()[0].id;
+
 console.log(facade.findAllQuestions());
-console.log(facade.findAllQuestions()[0].id);
-console.log(firstQuestionId);
+//console.log(facade.findAllQuestions()[0].id);
+//console.log(firstQuestionId);
+facade.removeQuestion();
+console.log(facade.findAllQuestions());
